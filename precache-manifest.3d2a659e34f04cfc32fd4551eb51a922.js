832dd7c851d3663f5c5e6c4abd099693c04a69eb
Kpoke <danielolojakpoke@gmail.com>
2020-09-25 02:25:24 +0100

    Updates

---
+++ precache-manifest.3d2a659e34f04cfc32fd4551eb51a922.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9599b8952556f55472432c9218dbca53",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "f470828a83bd4d55193f",
+    "url": "/tictactoe/static/css/main.e66fa53b.chunk.css"
+  },
+  {
+    "revision": "0831ed49ee73c737ea42",
+    "url": "/tictactoe/static/js/2.9db01ef1.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/tictactoe/static/js/2.9db01ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f470828a83bd4d55193f",
+    "url": "/tictactoe/static/js/main.00987724.chunk.js"
+  },
+  {
+    "revision": "4434fef27fe19069e69b",
+    "url": "/tictactoe/static/js/runtime-main.666fab5a.js"
+  }
+]);
